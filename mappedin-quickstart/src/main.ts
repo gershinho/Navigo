e652e8d20fbb9910237da68850c2496c179c8eff
eggshaan <eshaan0507@gmail.com>
2025-03-26 16:17:26 -0500

    initial wayfinding

--- mappedin-quickstart/src/main.ts
+++ mappedin-quickstart/src/main.ts
@@ -1,17 +1,52 @@
-import { getMapData, show3dMap } from '@mappedin/mappedin-js';
+import { getMapData, show3dMap, MapView, MapData, TGetMapDataOptions, Space } from '@mappedin/mappedin-js';
 import '@mappedin/mappedin-js/lib/index.css';
 
-// See Demo API key Terms and Conditions
-// /docs/demo-keys-and-maps
-const options = {
-	key: 'mik_yeBk0Vf0nNJtpesfu560e07e5',
-	secret: 'mis_2g9ST8ZcSFb5R9fPnsvYhrX3RyRwPtDGbMGweCYKEq385431022',
-	mapId: '65c0ff7430b94e3fabd5bb8c',
+// Mappedin API credentials and map ID
+const options: TGetMapDataOptions = {
+  key: 'mik_VVPjnAIXcr8qoGMTc5512078f',
+  secret: 'mis_KXM6oIhKkPzTSD3f2Y9fTuP9GAw0zfDvlve3xRRrQyq850e107b',
+  mapId: '677733b351f079000b7dcf62',
 };
-
 async function init() {
-	const mapData = await getMapData(options);
-	const mapView = await show3dMap(document.getElementById('mappedin-map') as HTMLDivElement, mapData);
+  const mapData: MapData = await getMapData(options);
+  const mapView: MapView = await show3dMap(document.getElementById('mappedin-map') as HTMLDivElement, mapData);
+  const firstSpace = mapData
+    .getByType('space')
+    .find((s) => s.name === 'Terminal 2') as Space;
+
+  // Gate E Space
+  const secondSpace = mapData
+    .getByType('space')
+    .find((s) => s.name === 'G gate') as Space;
+
+  // Label terminal 2 and gate e spaces
+  
+  
+  mapView.on('click', async (event) => {
+    const clickedLocation = event.coordinate;
+    const destination = mapData.getByType('space').find((s) => s.name === 'G gate');
+  
+    // If the destination is found, navigate to it.
+    if (destination) {
+      //Ensure that directions could be generated (user clicked on a navigable space).
+      const directions = mapData.getDirections(clickedLocation, destination);
+  
+      if (directions) {
+        // Navigate from the clicked location to the gymnasium.
+        mapView.Navigation.draw(directions, {
+          pathOptions: {
+            nearRadius: 1,
+            farRadius: 1,
+          },
+        });
+      }
+    }
+  });
+
+  mapView.Labels.add(firstSpace, firstSpace.name);
+  mapView.Labels.add(secondSpace, secondSpace.name);
+
 }
+
 
 init();
